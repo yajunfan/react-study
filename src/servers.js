46d872b26ecef6d4ccc538fdbd3e914addaa479b
fanyajun <fanyajun@pwrd.com>
2019-12-06 15:19:59 +0800

    react 的基础知识学完，2019.12.6

--- src/servers.js
+++ src/servers.js
@@ -1,7 +1,9 @@
 import React,{Component,Fragment} from 'react'
-
-
-
+import ServerItem from './serverItem'
+import Teacher from './teacher'
+import axios from 'axios'
+import {CSSTransition,TransitionGroup} from 'react-transition-group'
+import './App.css'
 class Servers extends Component{
     constructor(props){
         super(props);
@@ -33,17 +35,61 @@ class Servers extends Component{
                     <br/>
                     6.JSX中标签 label 中的for在jsx语法中不可以写for，不影响页面，但是控制台会爆红，因为和javascript中的for循环混淆，所有要使用htmlfor；然后实现点击label激活对应的input <br/>
                     <br/>
+                    7.React的特性中有一个概念叫做“单项数据流”,指的是数据不像vue是双向数据绑定，比如我们对inut进行change的时候，对应的inputValue是不会改变的，他是只读的，<br/>
+                      我们需要通过setState的方式对他进行重新赋值；在vue中，input改变，对应的inputValue的值也跟着改变，这是两者之间的区别<br/>
+                      <br/>
+                    8. React中标签上的事件，都需要改变this指向，组件内置的方法中的this为组件对象，在组件中自定义的方法中的this为null<br/>
+                        必须强制进行指向，使用bind(this)改变this指向<br/>
+                    <br/>
+                    9. 在谷歌商店中添加react-devloper-tool，新版的控制面板中不再是react选项，而是components选项，可以通过点击【尖括号】这个选项看到代码<br/>
+                    <br/>
+                    10. React中可以使用ref来绑定dom元素，如下面得ul标签上绑定得ref=xxx; 在任何函数中console.log(this.ul)，都可以获取到ul这个元素<br/>
+                        也可以和vue一样，ref="refName" ，在函数中使用this.refs.refName得形式，一般不推荐使用<br/>
+                    <br/>
+                     11. React得四个大的生命周期分别得是 Initialization(初始化阶段)、Mounting(挂载阶段)、Updation(更新阶段)、Unmounting(销毁阶段) <br/>
+                     Mounting中又包含三个小生命周期，分别是componentWillMount(组件即将被挂载阶段，只页面刷新时执行一次)、render(页面state或者props发生变化)、componentDidMount(挂载完成，只页面刷新时执行一次)<br/>
+                     Updation中包含两种，state和props得更新，分别包含三个小生命周期，但是中间会穿插render阶段分别是<br/>
+                     1）. shouldComponentUpdate(询问是否允许更新，返回true就是允许，返回false就不允许,页面上不可修改数据，比如input不可输入等)<br/>
+                     2）. componentWillUpdate(在组件更新之前执行，前提是允许更新)<br/>
+                     3）. render(挂在渲染)<br/>
+                     4）. componentDidUpdate(组件更新完成)<br/>
+                     以上是state得更新，下面是props得更新<br/>
+                     1）. componentWillReceiveProps(这个函数是在子组件中，因为子组件中才会接受props，子组件接收到父组件传递过来的参数，父组件render函数重新被执行，这个生命周期就会被执行。)<br/>
+                     2）.componentWillUnmount (函数出现在子组件中，当子组件从父页面移除的时候，触发) 
+                     <br/>
+                     12. 
                 </div>
                 <br/>
+                
                 <div><input value={this.state.inputValue} onChange={this.changeValue.bind(this)}/><button onClick={this.addStudy.bind(this)}>增加课程</button></div>
-                <ul>
-                    {
-                        this.state.lists.map((item,index)=>{
-                            return <li key={index} onClick={this.deleteStudy.bind(this,index)} dangerouslySetInnerHTML={{__html:item.name}}></li>
-                        })
-                    }
-                </ul>
-            
+                {/* 引入组件 */}
+                {/* <ServerItem content={this.state.inputVlaue2} othercontent={this.state.inputValue} /> */}
+                
+                    <ul ref={(ul)=>this.ul=ul}>
+                    <TransitionGroup>
+                        {
+                            // 返回的html必须跟在return后面，否则报错
+                            this.state.lists.map((item,index)=>{
+                                // onClick={this.deleteStudy.bind(this,index)} 
+                                return (
+                                    // <li  key={index}  >
+                                        <CSSTransition
+                                            timeout={1000}
+                                            classNames='boss-text'
+                                            unmountOnExit
+                                            appear={true}  key={index}  >
+                                            {/* <span dangerouslySetInnerHTML={{__html:item.name}}></span> */}
+                                    {/* 引入组件 */}
+                                            <ServerItem content={item.name} index={index} deleteItem={this.deleteStudy.bind(this,index)}/>
+                                        </CSSTransition>
+                                    //  </li> 
+                                )
+                            })
+                        }
+                        </TransitionGroup>
+                    </ul>
+                
+                <Teacher/>
             </Fragment> 
         )
     }
@@ -54,6 +100,9 @@ class Servers extends Component{
         this.setState({
             inputValue:e.target.value ,
             inputVlaue2:e.target.value 
+        },()=>{
+           
+            console.log("这个回调是解决 由于setState函数是一个异步函数，会导致出现没等dom渲染完就打印出不完整得数据，这个回调会在数据全部渲染完了才会执行")
         });
        
     }
@@ -69,6 +118,20 @@ class Servers extends Component{
         this.setState({
             lists:lists
         });
+    }
+    getData(){
+        //接口可以使用easy-mock进行模拟，但该网站经常挂掉
+        axios.post('https://web-api.juejin.im/v3/web/wbbr/bgeda').then((res)=>{
+            console.log(res)
+        }).catch((error)=>{
+            console.log(error)
+        })
+    }
+    shouldComponentUpdate(){
+        return true;
+    }
+    componentDidMount(){
+        this.getData();
     }
 }
 export default Servers
